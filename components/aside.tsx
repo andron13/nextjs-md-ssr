ba995289e5578b1f103654f9e2201ad65efbd3dd
Tedzury <asdrogachev@gmail.com>
2023-10-02 16:45:54 +0300

    init: add ts support to project

--- components/aside.tsx
+++ components/aside.tsx
@@ -1,6 +1,8 @@
 // Created on 27.07.23 by 19:21:
 
-const Aside = () => {
+import React from 'react';
+
+function Aside() {
   return (
     <aside className="bg-gray-200 shadow-md rounded-lg p-4 my-1 mr-1 w-full sm:col-start-5 sm:col-end-6">
       <h2 className="text-xl font-bold mb-4">Меню</h2>
@@ -17,6 +19,6 @@ const Aside = () => {
       </ul>
     </aside>
   );
-};
+}
 
 export default Aside;
