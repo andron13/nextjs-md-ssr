ec27ad0a75a31fd216c92cd27bbd1e0e11d9f648
Bogdan <thegarrybit@gmail.com>
2023-10-19 16:50:05 +0300

    refactor: change button outline

--- components/button.tsx
+++ components/button.tsx
@@ -1,8 +1,8 @@
 import { memo, ReactNode } from 'react';
 
 const buttonTypes = {
-  filled: 'rounded-md bg-black-900 px-6 py-[14px] text-white transition-colors hover:bg-black',
-  empty: 'rounded-md px-4 py-2 transition-all hover:bg-black-100',
+  filled: 'rounded-md bg-black-900 px-6 py-[14px] text-white hover:bg-black',
+  empty: 'rounded-md px-4 py-2 hover:bg-black-100',
 } as const;
 
 interface IButtonProps {
@@ -18,7 +18,9 @@ const Button = memo(function Button({
   onClick,
   className,
 }: IButtonProps) {
-  const classes = `${buttonTypes[type]} ${className ?? ''}`;
+  const classes = `transition-all focus:border-transparent focus:outline-none focus:ring focus:ring-accent-600 focus:ring-opacity-50 ${
+    buttonTypes[type]
+  } ${className ?? ''}`;
 
   return (
     <button onClick={onClick} className={classes}>
