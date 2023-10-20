1f58599bf3e4b2a75597f54e63e3accd47ee75d0
Bogdan <thegarrybit@gmail.com>
2023-10-20 10:11:11 +0300

    refactor: change search input styles

--- components/searchInput/searchInput.tsx
+++ components/searchInput/searchInput.tsx
@@ -10,14 +10,10 @@ export default function SearchInput() {
 
   return (
     <div className="mt-6 w-full px-5 sm:absolute sm:right-[25px] sm:top-[10px] sm:mt-0 sm:w-[280px] sm:px-0">
-      <label
-        htmlFor="searchInp"
-        className="flex gap-[6px] rounded-md px-3 py-2 text-accent-secondary-700 shadow-[0px_1px_2px_2px_rgba(239,239,239,1)]"
-      >
-        <Image src={searchIcon} alt="" />
+      <label htmlFor="searchInp" className="flex gap-[6px]">
+        <Image className="absolute bottom-0 left-3 top-0 my-auto" src={searchIcon} alt="" />
         <input
-          className="w-full text-sm"
-          type="text"
+          className="w-full rounded-md px-3 py-2 pl-10 text-sm font-medium text-black shadow-[0px_1px_2px_2px_rgba(239,239,239,1)] transition-all placeholder:text-accent-secondary-400 focus:border-transparent focus:outline-none focus:ring focus:ring-accent-600 focus:ring-opacity-50"
           id="searchInp"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
