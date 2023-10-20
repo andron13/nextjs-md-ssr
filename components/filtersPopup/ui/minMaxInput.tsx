8723d0b0367c758e72ea0364bd1a1df2717f43a2
Bogdan <thegarrybit@gmail.com>
2023-10-20 15:09:18 +0300

    fix: error

--- components/filtersPopup/ui/minMaxInput.tsx
+++ components/filtersPopup/ui/minMaxInput.tsx
@@ -13,9 +13,8 @@ function MinMaxInput({ text, onUpdate, values }: IMinMaxInputProps) {
     <div className="py-6">
       <span className="text-black">{text}</span>
       <div className="mt-5 flex items-center gap-2">
-        <Input onChange={(min) => onUpdate(min, max)} type="number" placeholder="Min" value={min} />{' '}
-        —{' '}
-        <Input onChange={(max) => onUpdate(min, max)} type="number" placeholder="Max" value={max} />
+        <Input onChange={(min) => onUpdate(min, max)} type="number" placeholder="Min" val={min} /> —{' '}
+        <Input onChange={(max) => onUpdate(min, max)} type="number" placeholder="Max" val={max} />
       </div>
     </div>
   );
